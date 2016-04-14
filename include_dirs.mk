a72c9d82c4122fd2f13b7f54ade8d56ec551b81a
Yogesh Pande <Yogesh.Pande@arm.com>
2016-04-14 17:05:26 +0300

    Removing unnecessary include paths from include_dirs.mk file

--- include_dirs.mk
+++ include_dirs.mk
@@ -2,15 +2,8 @@ INCLUDE_DIRS := \
 	../ \
 	. \
 	../../nsdl-c \
-	../../libService/libService \
 	../../mbed-trace \
-	../../libService/exported-libs/mbed-client-libservice \
-	../../libService \
-	../../mbedtls/include \
-	../../event-loop/nanostack-event-loop \
-	../../nanostack/nanostack \
         ../lwm2m-client \
         ../lwm2m-client-linux/source \
-        ../lwm2m-client-mbedtls \
         source \
 
